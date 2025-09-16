0b85ed1c344d220e91b529be3394478437aff179
Aditya Sharma <167906851+77adityasharma@users.noreply.github.com>
2025-09-16 13:45:41 +0530

    boilerplate

---
+++ boilerplate.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll mod = 1e9+7;
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    ll t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    // solve();
+
+    return 0;
+}
